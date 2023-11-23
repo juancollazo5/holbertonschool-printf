962f09d2efa60f442a067870fdfafa5a8206d202
osvaldo antompietri <antompietriosvaldo@gmail.com>
2023-11-22 23:14:29 -0400

    All the libraries and prototypes

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+/*Function Prototypes*/
+int _printf(const char *format, ...);
+void s_case(va_list args, int *count);
+void c_case(va_list args, int *count);
+int _strlen(char *s);
+#endif
