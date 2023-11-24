a7655f5b5f650a71e9ae4252811293a9a9ebb030
juancollazo5 <juancollazo577@outlook.com>
2023-11-24 00:53:32 +0000

    function update
    update function for add case u, 0, x, X, p, r

--- _printf.c
+++ _printf.c
@@ -33,9 +33,34 @@ int _printf(const char *format, ...)
                 c_case(args, &count);
                 break;
 
-            case 'd': /* case of a integer */
+            case 'd': /* case of an integer */
             case 'i':
                 count += _print_int(va_arg(args, int));
+                break;
+
+            case 'u': /* case of an unsigned integer */
+                count += _print_int(va_arg(args, unsigned int));
+                break;
+
+            case 'o': /* case of an unsigned octal */
+                count += _print_octal(va_arg(args, unsigned int));
+                break;
+
+            case 'x': /* case of an unsigned hexadecimal (lowercase) */
+                count += _print_hex(va_arg(args, unsigned int), 0);
+                break;
+
+            case 'X': /* case of an unsigned hexadecimal (uppercase) */
+                count += _print_hex(va_arg(args, unsigned int), 1);
+                break;
+
+            case 'p':
+                count += _print_ptr(va_arg(args, void *));
+                break;
+
+            case 'r':
+                write(1, &format[i - 1], 2);
+                count += 2;
                 break;
 
             default:
