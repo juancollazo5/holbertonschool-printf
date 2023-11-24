0351f29088c50e7b4fa303f00e0c9a854ff233c5
juancollazo5 <juancollazo577@outlook.com>
2023-11-24 00:49:42 +0000

    functions update
    add function for case u, 0, x, X, p, r

--- functions.c
+++ functions.c
@@ -52,3 +52,100 @@ int _strlen(char *s)
 
     return (str);
 }
+
+/**
+ * _print_octal - Prints an unsigned integer in octal format
+ * @n: The unsigned integer to be printed
+ * Return: The number of characters printed
+ */
+int _print_octal(unsigned int n)
+{
+    int i = 0, j;
+    char buffer[20];
+
+    if (n == 0)
+    {
+        write(1, "0", 1);
+        return 1;
+    }
+
+    while (n != 0)
+    {
+        buffer[i++] = n % 8 + '0';
+        n /= 8;
+    }
+
+    for (j = i - 1; j >= 0; j--)
+    {
+        write(1, &buffer[j], 1);
+    }
+
+    return i;
+}
+
+/**
+ * _print_hex_digit - Converts a hexadecimal digit to its ASCII character
+ * @digit: The hexadecimal digit (0-15)
+ * @uppercase: Flag indicating whether to print in uppercase (1) or lowercase (0)
+ * Return: The ASCII character representing the hexadecimal digit
+ */
+int _print_hex_digit(int digit, int uppercase)
+{
+    if (digit < 10)
+    {
+        return digit + '0';
+    }
+    else
+    {
+        return (uppercase ? 'A' : 'a') + (digit - 10);
+    }
+}
+
+/**
+ * _print_hex - Prints an unsigned integer in hexadecimal format
+ * @n: The unsigned integer to be printed
+ * @uppercase: Flag indicating whether to print in uppercase (1) or lowercase (0)
+ * Return: The number of characters printed
+ */
+int _print_hex(unsigned int n, int uppercase)
+{
+    int i = 0, j;
+    char buffer[20];
+
+    if (n == 0)
+    {
+        write(1, "0", 1);
+        return 1;
+    }
+
+    while (n != 0)
+    {
+        buffer[i++] = _print_hex_digit(n % 16, uppercase);
+        n /= 16;
+    }
+
+    for (j = i - 1; j >= 0; j--)
+    {
+        write(1, &buffer[j], 1);
+    }
+
+    return i;
+}
+
+/**
+ * _print_ptr - Prints a pointer address in hexadecimal format
+ * @ptr: The pointer to be printed
+ * Return: The number of characters printed
+ */
+int _print_ptr(void *ptr)
+{
+    unsigned long int value = (unsigned long int)ptr;
+    int count = 0;
+    
+    write(1, "0x", 2);
+    count += 2;
+    
+    count += _print_hex(value, 1);
+    
+    return count;
+}
