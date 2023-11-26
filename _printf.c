d12b795796e81e3381e417f3dcce86d3eec68768
juancollazo5 <juancollazo577@outlook.com>
2023-11-26 04:41:24 +0000

    function update
    update call function for case u

--- _printf.c
+++ _printf.c
@@ -34,12 +34,12 @@ int _printf(const char *format, ...)
                 break;
 
             case 'd': /* case of an integer */
-            case 'i':
+	    case 'i':
                 count += _print_int(va_arg(args, int));
                 break;
 
             case 'u': /* case of an unsigned integer */
-                count += _print_int(va_arg(args, unsigned int));
+                count +=  _print_unsigned(va_arg(args, unsigned int));
                 break;
 
             case 'o': /* case of an unsigned octal */
